ae1ecb3e32e995885f1d1a4342b4db37ea370d58
Eduardo Blanco M. <zener_servitec@hotmail.com>
2025-09-23 20:56:22 -0300

    geracoes

--- js/init.js
+++ js/init.js
@@ -3,6 +3,8 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.collapsible').collapsible();
+    $('.tabs').tabs();
 	
   }); // end of document ready
 })(jQuery); // end of jQuery name space
